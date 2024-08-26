e4b2d58dea47846debff50d229cd1841fc5feaa8
Ilie1985 <ilie.marian32@yahoo.com>
2024-08-26 20:20:53 +0100

    check if the commit works

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -3,3 +3,7 @@ city = input("Whats the name of the city you grew up in? \n")
 pet_name = input("What's your pet's name? \n")
 print("Your band name could be " + city + " " + pet_name)
 print(" Well done ,day 1 completed")
+print(" Well done ,day 1 completed")
+print(" Well done ,day 1 completed")
+
+
