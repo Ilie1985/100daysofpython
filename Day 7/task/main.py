92189c6c1323f7eb3febb356442c5a1b558e48c3
Ilie1985 <ilie.marian32@yahoo.com>
2024-09-09 17:44:40 +0100

    day 7 step 3 completed

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -12,21 +12,28 @@ print(placeholder)
 
 # TODO-1: - Use a while loop to let the user guess again.
 
-guess = input("Guess a letter: ").lower()
 
-display = ""
 
-while display != chosen_word:
-    if display != chosen_word:
+game_over = False
+correct_letters = []
 
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+    display = ""
 
 
 # TODO-2: Change the for loop so that you keep the previous correct letters in display.
 
-for letter in chosen_word:
-    if letter == guess:
-        display += letter
-    else:
-        display += "_"
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(letter)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
 
-print(display)
+    print(display)
+    if "_" not in display:
+        game_over = True
+        print("You win.")
