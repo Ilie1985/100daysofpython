07ee40dea24f5b266c77b6884095a875df5ff07c
Ilie1985 <ilie.marian32@yahoo.com>
2024-09-29 19:48:45 +0100

    day 8 functions and  ,caesar cipher complete

--- Day 8/Caesar Cipher 3/main.py
+++ Day 8/Caesar Cipher 3/main.py
@@ -9,38 +9,47 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 def caesar(original_text, shift_amount, encode_or_decode):
     output_text = ""
+    decrypted_text = ""
 
     for letter in original_text:
         if encode_or_decode == "decode":
-            shift_amount *= -1
+            shifted_position = alphabet.index(letter) - shift_amount
+            decrypted_text += alphabet[shifted_position]
 
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        output_text += alphabet[shifted_position]
-    print(f"Here is the {encode_or_decode}d result: {output_text}")
+        else:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
 
+
+    if encode_or_decode == "decode":
+        print(f"Here is the decoded result: {decrypted_text}")
+    elif encode_or_decode == "encode":
+        print(f"Here is the {encode_or_decode}d result: {output_text}")
 
 # TODO-3: Can you figure out a way to restart the cipher program?
+try_again = True
+
+while try_again:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+
+    if text not in alphabet:
+        for letter in text:
+            if letter not in alphabet:
+                alphabet.append(letter)
+                print(alphabet)
 
 
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+    try_again = False
+    play = input("Type yes if you want to go again, no if you dont!").lower()
 
-if text not in alphabet:
-    for letter in text:
-        if letter not in alphabet:
-            alphabet.append(letter)
-            print(alphabet)
-
-
-caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
-
-try_again=input("Type yes if you want to go again, no if you dont!").lower()
-
-if try_again == "yes":
-    print("yes")
-
-
+    if play == "no":
+        try_again = False
+        print("Goodbye")
+    elif play == "yes":
+        try_again = True
