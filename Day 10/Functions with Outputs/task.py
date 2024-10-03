73f561d4028f6ef9a746cfdf4b77b350c3e1ea4c
Ilie1985 <ilie.marian32@yahoo.com>
2024-10-03 16:43:21 +0100

    day 10 functions with outputs, multiple return values

---
+++ Day 10/Functions with Outputs/task.py
@@ -0,0 +1,7 @@
+def format_name(f_name, l_name):
+	forename = f_name.title()
+	last_name = l_name.title()
+	return forename, last_name
+
+print(format_name("marian", "ilie"))
+
--- Day 10/Multiple Return Values/task.py
+++ Day 10/Multiple Return Values/task.py
@@ -4,4 +4,23 @@ def format_name(f_name, l_name):
     return f"{formated_f_name} {formated_l_name}"
 
 
-print(format_name("AnGEla", "YU"))
+print(format_name("romeo", "fantastik"))
+
+def canBuyAlcohol(age):
+    if age >= 18:
+        return True
+    else:
+        return False
+print(canBuyAlcohol(18))
+
+
+def canBuyAlcohol(age):
+    # If the data type of the age input is not an int, then exit.
+    if type(age) != int:
+        return
+
+    if age >= 18:
+        return True
+    else:
+        return False
+print(canBuyAlcohol("two"))
