bd9ca921f36f514a50bb91fa84036e2438881077
Ilie1985 <ilie.marian32@yahoo.com>
2024-11-08 19:44:14 +0000

    day 12 number guessing project

--- Day 12/Number Guessing Project/main.py
+++ Day 12/Number Guessing Project/main.py
@@ -2,10 +2,43 @@ import random
 
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100")
+list_of_num = range(1, 101)
+number_to_be_guessed = random.choice(list_of_num)
+print(number_to_be_guessed)
+guess_again = True
+
+
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard'").lower()
+if difficulty == "easy":
+	attempts = range(10)
+	attempts_left = 10
+	while guess_again:
+		chosen_number_on_easy = int(input("You have 10 attempts to guess the number,please make a guess:"))
+		if chosen_number_on_easy != number_to_be_guessed:
+			for attempt in attempts:
+				while attempts_left != 0:
+					attempts_left -= 1
+					if attempts_left == 0:
+						print(f"""You have failed to guess the number,
+	the number you had to guess was {number_to_be_guessed}""")
+						attempts_left = 0
+						guess_again = False
+			if chosen_number_on_easy > number_to_be_guessed:
+				print(f"""You have {attempts_left} attempts left,
+	the chosen number is high, please guess again""")
+			elif chosen_number_on_easy < number_to_be_guessed:
+				print(f"""You have {attempts_left} attempts left,
+	the chosen number is low, please guess again""")
+			elif chosen_number_on_easy == number_to_be_guessed:
+				print(f"""Well done the number that 
+	you had to guess was indeed {number_to_be_guessed}""")
 
-def num_to_be_guessed():
 
-	list_of_num = range(1, 101)
-	n = random.choice(list_of_num)
-	return n
+	# elif difficulty == "hard":
+	# 	chosen_number_on_hard = input("You have 5 attempts to guess the number,please make a guess:")
+	# else:
+	# 	print("Please type easy or hard to play the game")
+
+
+
+
